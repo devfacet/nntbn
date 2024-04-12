486ef4d1285f592e6b1404d3314a2877293d0429
Fatih Cetinkaya <965295+devfacet@users.noreply.github.com>
2024-04-12 19:48:18 -0400

    Make nn_dot_product function arguments fixed array

--- include/nn_dot_product.h
+++ include/nn_dot_product.h
@@ -1,13 +1,17 @@
 #ifndef NN_DOT_PRODUCT_H
 #define NN_DOT_PRODUCT_H
 
-#include "nn_error.h"
 #include <stddef.h>
 
+// NN_DOT_PRODUCT_MAX_VECTOR_SIZE defines the maximum size of a vector.
+#ifndef NN_DOT_PRODUCT_MAX_VECTOR_SIZE
+#define NN_DOT_PRODUCT_MAX_VECTOR_SIZE 64
+#endif
+
 // NNDotProductFunction represents a function that calculates the dot product of two vectors.
-typedef float (*NNDotProductFunction)(const float *a, const float *b, size_t length);
+typedef float (*NNDotProductFunction)(const float a[NN_DOT_PRODUCT_MAX_VECTOR_SIZE], const float b[NN_DOT_PRODUCT_MAX_VECTOR_SIZE], size_t vector_size);
 
 // nn_dot_product calculates the dot product of two vectors.
-float nn_dot_product(const float *a, const float *b, size_t length);
+float nn_dot_product(const float a[NN_DOT_PRODUCT_MAX_VECTOR_SIZE], const float b[NN_DOT_PRODUCT_MAX_VECTOR_SIZE], size_t vector_size);
 
 #endif // NN_DOT_PRODUCT_H
